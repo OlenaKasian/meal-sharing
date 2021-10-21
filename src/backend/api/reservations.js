515264343a3a47288d0054a6068c6b4a6577e7f2
OlenaKasian <81156179+OlenaKasian@users.noreply.github.com>
2021-10-21 11:58:09 +0200

    reservations

---
+++ src/backend/api/reservations.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const knex = require("../database");
+
+router.get("/", async (request, response) => {
+    try {
+        const reservations = await knex("reservations");
+        response.json(reservations);
+    } catch (e) {
+        throw e;
+    }
+});
+
+router.post("/", async (request, response) => {
+    try {
+        const reservations = await knex("reservations").insert(request.body);
+        response.send('it added');
+    } catch (e) {
+        throw e;
+    }
+});
+
+router.get("/:id", async (request, response) => {
+    try {
+        const reservations = await knex("reservations").where('id', parseInt(request.params.id));
+        response.json(reservations);
+    } catch (e) {
+        throw e;
+    }
+});
+
+router.put("/:id", async (request, response) => {
+    try {
+        const reservations = await knex("reservations").where('id', parseInt(request.params.id)).update(request.body);
+        response.json(reservations);
+    } catch (e) {
+        throw e;
+    }
+});
+
+router.delete("/:id", async (request, response) => {
+    try {
+        const reservations = await knex("reservations").where('id', parseInt(request.params.id)).del();
+        response.json(reservations);
+    } catch (e) {
+        throw e;
+    }
+});
+
+
+module.exports = router;

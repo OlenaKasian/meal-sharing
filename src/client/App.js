51b6444964bf54d28db0f1f6522ac9a7c1f022df
OlenaKasian <81156179+OlenaKasian@users.noreply.github.com>
2021-10-25 11:43:14 +0200

    Update App.js

--- src/client/App.js
+++ src/client/App.js
@@ -1,21 +1,44 @@
 import React from "react";
+import Header from "./components/MainPage/Header";
+
+import MealList from "./components/MainPage/MealList";
+import Footer from "./components/MainPage/Footer"
+import Home from "./components/MainPage/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
 
 function App() {
+  const [meals, setMeals] = React.useState([]);
+  const [reviews, setReviews] = React.useState([]);
+  React.useEffect(() => {
+    fetch("/api/meals")
+      .then((res) => res.json())
+      .then((data) => {
+        setMeals(data);
+      });
+    fetch("/api/reviews")
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data);
+      });
+  }, []);
+
   return (
-    <Router>
+    <Router><Header />
       <Route exact path="/">
-        <p>test</p>
+        <Home meals={meals} reviews={reviews} />
       </Route>
-      <Route exact path="/lol">
-        <p>lol</p>
+      <Route exact path="/meals">
+        <MealList meals={meals} />
       </Route>
-      <Route exact path="/test-component">
-        <TestComponent></TestComponent>
-      </Route>
+     
+
+
+      <Footer />
     </Router>
+
   );
 }
 
 export default App;
+

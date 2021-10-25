12fd793610a1b6bd74f81784db7050786080ba67
OlenaKasian <81156179+OlenaKasian@users.noreply.github.com>
2021-10-25 16:28:20 +0200

    Update Home.js

--- src/client/components/MainPage/Home.js
+++ src/client/components/MainPage/Home.js
@@ -17,30 +17,20 @@ const Home = (props) => {
     }, [search])
 
     return (<>
-
-
         <div><hr />
             <input type='text' placeholder="search for meal" onChange={(e) => setSearch(e.target.value)} />
             <hr />
             <div>
-
                 {meals.map((meal) => (
                     <div key={meal.id}>
-
                         <h3>{meal.title}</h3>
                         <p>{meal.description}</p>
                         <button><Link to={`/meals/${meal.id}`}>More Details</Link></button>
-
                     </div>
-
-
                 ))}
             </div>
         </div>
-
-
         <img src={bg} alt="background-image" className="center" />
-
     </>
     );
 };

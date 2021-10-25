245fb403374c56740b6e389642c9fc4725b40d02
OlenaKasian <81156179+OlenaKasian@users.noreply.github.com>
2021-10-25 12:01:21 +0200

    Update MealList.js

--- src/client/components/MainPage/MealList.js
+++ src/client/components/MainPage/MealList.js
@@ -1,17 +1,18 @@
 import React from "react";
+import selectFood from './../../assets/images/selectfood.png';
 
 function MealList() {
     return (
         <nav>
             <div>
-                <img src="src/frontend/assets/images/select food.jpg" alt="meal photo" id="meal-photo" />
+                <img src={selectFood} alt="meal photo" id="meal-photo" />
             </div>
             <div className="container">
                 <h2>Name1 food</h2>
                 <p>Very good food</p>
                 <p>Location: Denmark </p>
                 <p>Price: 120 DKK</p>
-                <a class="btn-link bgn-green" href="////">Book Meal</a>
+                <a className="btn-link bgn-green" href="////">Book Meal</a>
             </div>
         </nav >
 

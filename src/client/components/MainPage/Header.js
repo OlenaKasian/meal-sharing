3ce3d7360509480e19bc74818694ab2e50eaeb56
OlenaKasian <81156179+OlenaKasian@users.noreply.github.com>
2021-10-27 13:24:55 +0200

    Update Header.js

--- src/client/components/MainPage/Header.js
+++ src/client/components/MainPage/Header.js
@@ -5,20 +5,22 @@ import logo from "./../../assets/images/logo.png";
 
 function Header() {
     return (
-        <nav>
-            <div className="topnav" id="myTopnav">
-                {/* <a href="#home" className="active">Home</a>
+
+
+        <div className="topnav" id="myTopnav">
+            {/* <a href="#home" className="active">Home</a>
                 <a href="#news">News</a>
                 <a href="#contact">Contact</a>
                 <a href="#about">About</a>
                 <a href="javascript:void(0);" className="icon" onclick="myFunction()">
                     <i className="fa fa-bars"></i>
                 </a> */}
-                <Link to="/"><img src={logo} alt="logo" id="logo" width={25} />Meal Sharing</Link>
-                <Link to="/meals">Find meals</Link>
-                <Link to="/about">About us</Link>
-            </div>
-        </nav >
+            <Link to="/"><img src={logo} alt="logo" id="logo" width={25} />Meal Sharing</Link>
+            <Link to="/meals">Find meals</Link>
+            <Link to="/newmeal">New Meal</Link>
+            <Link to="/about">About us</Link>
+        </div>
+
 
     );
 }

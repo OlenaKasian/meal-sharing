3ffa77a523553b615a2b46696be71d4476d5e19c
OlenaKasian <81156179+OlenaKasian@users.noreply.github.com>
2021-10-27 10:14:16 +0200

    Update Home.js

--- src/client/components/MainPage/Home.js
+++ src/client/components/MainPage/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import bg from './../../assets/images/mainbackround.png'
+import "./Home.css"
 
 const Home = (props) => {
     const [search, setSearch] = useState("")
@@ -17,20 +18,30 @@ const Home = (props) => {
     }, [search])
 
     return (<>
+
+
         <div><hr />
             <input type='text' placeholder="search for meal" onChange={(e) => setSearch(e.target.value)} />
             <hr />
             <div>
+
                 {meals.map((meal) => (
                     <div key={meal.id}>
+
                         <h3>{meal.title}</h3>
                         <p>{meal.description}</p>
                         <button><Link to={`/meals/${meal.id}`}>More Details</Link></button>
+
                     </div>
+
+
                 ))}
             </div>
         </div>
-        <img src={bg} alt="background-image" className="center" />
+
+
+        <img src={bg} alt="background-image" className="imagePic" />
+
     </>
     );
 };
